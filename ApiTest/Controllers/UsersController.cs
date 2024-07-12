a731c8d3aa4a6d760151050c6ca04ba9a53358e4
edwinforss <edwin.forss@codeitab.se>
2024-07-12 11:40:50 +0200

    Users get works

---
+++ ApiTest/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace ApiTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET api/<USersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST api/<USersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<USersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<USersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApiTest/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTest
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string HCP { get; set; }
+        public string Password { get; set; }
+    }
+}
